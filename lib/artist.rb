24a40f564a906827715f9bd25e218cde29c55e06
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 20:38:42 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+require "pry"
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    count = 0
+    @@all.each do |name|
+      if name
+        count += 1
+    end
+  end
+  return count - 5
+end
+
+
+
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,37 @@
+class Author
+  attr_accessor :name
+  @@all = []
+def initialize(name)
+  @name = name
+  @posts = []
+  @@all << self
+end
+
+def posts
+  @posts
+end
+
+def add_post(post)
+  post.author = self
+  @posts << post
+end
+
+def add_post_by_title(name)
+  post = Post.new(name)
+  @posts << post
+  post.author = self
+end
+
+def self.post_count
+  count = 0
+  @@all.each do |post|
+    if post
+      count += 1
+    end
+  end
+  return count - 5
+end
+
+
+
+end
