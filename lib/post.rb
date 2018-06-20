afa5f5a7f53deb2ec7b210351da2265688b88ede
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 19:22:17 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,10 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
   @@all = []
   def initialize(title)
     @title = title
-    @@all << title
+    @author = author
+    @@all << self.title
   end
 
   def all
